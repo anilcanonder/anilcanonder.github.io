a6bb95d53d8922c20aa79fa26dd2d8554cc098bf
anilcanonder <52328426+anilcanonder@users.noreply.github.com>
2025-09-15 14:47:29 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    setTimeout(function(){
+        $('.loader-container').addClass('fadeaway');
+    }, 3000);
+
+    });
+
+$(document).ready(function() {
+
+    setTimeout(function(){
+        $('.loader-container').toggleClass('loaded');
+    }, 3900);
+  });
